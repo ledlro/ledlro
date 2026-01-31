dadad4e5f3bf6e6b7455b5d68ce334c937c1ddd4
LEDLRO.io <info.ledlro.io@gmail.com>
2026-02-01 01:14:58 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,28 @@
+import Link from 'next/link'
+import styles from '../styles/home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.logo}>LEDLRO.io</div>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/posts">Posts</Link>
+        </nav>
+      </header>
+
+      <main className={styles.main}>
+        <h1>LEDLRO.io</h1>
+        <p>Your brand for modern, minimal, dynamic web experiences.</p>
+        <Link href="/about" className={styles.btn}>Learn More</Link>
+      </main>
+
+      <footer className={styles.footer}>
+        Designed & Developed by LEDLRO.io
+      </footer>
+    </div>
+  )
+}
 
