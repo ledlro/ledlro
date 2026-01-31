cd92f937421c1bc4222b3f32cb7e5f0b6d337e0e
LEDLRO.io <info.ledlro.io@gmail.com>
2026-02-01 01:15:32 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,12 @@
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <div style={{padding:'2rem', fontFamily:'Arial, sans-serif', color:'#111'}}>
+      <h1>About LEDLRO.io</h1>
+      <p>We create minimal, dynamic, modern web solutions with black & white design aesthetics.</p>
+      <Link href="/">Back to Home</Link>
+    </div>
+  )
+}
 
